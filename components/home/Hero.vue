f9720140eb5deb35254509908beb8925ea913b38
jmacalawa <jmacalawa@futurex.ph>
2022-08-01 00:29:33 +0800

    first commit

---
+++ components/home/Hero.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <!-- <v-img
+      src="/images/hero-img.jpeg"
+      color="transparent"
+      height="85vh"
+      gradient="to top right, rgba(100,115,201,.33), rgba(0,0,0,0.1)"
+      class="d-flex justify-start align-center pl-16"
+    >
+
+    </v-img> -->
+    <v-sheet  class="d-flex justify-center align-center px-16" height="75vh">
+      <v-row class="px-16">
+        <v-col cols="6" align-self="center">
+          <div class="text-h2 secondary--text">
+            <b>E-Learning</b>
+          </div>
+          <!-- <div class="text-h5 black--text">
+            <b>
+              Staycation for BER Months!<br />
+              2022
+            </b>
+          </div> -->
+          <div class="my-10 white--text">
+            <v-btn depressed color="secondary" dark>Get Started </v-btn>
+          </div>
+        </v-col>
+        <v-col>
+          <v-img src="/images/e-learning.jpg" height="600" width="500" contain>
+                
+            </v-img>
+        </v-col>
+      </v-row>
+    </v-sheet>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
---
+++ components/messages/MessageContainer.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <v-row>
+        <v-col>
+            <div v-for="x in 10" :key="x">
+                <div>
+                    Juan Delacruz
+                </div>
+                <div>
+                    2020-10-10
+                </div>
+            </div>
+        </v-col>
+        <v-divider vertical />
+        <v-col>
+
+        </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
